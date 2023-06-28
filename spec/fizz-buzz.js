19669dd18697cf0aa65e1953f28645eeccf6165d
Paulo Jorge Junior <p4ulojorge@gmail.com>
2023-06-27 21:39:12 -0400

    Retorna Fizz se multiplo de 3

--- spec/fizz-buzz.js
+++ spec/fizz-buzz.js
@@ -1,4 +1,8 @@
+const Fizz = "Fizz";
+const Buzz = "Buzz";
+
 function fizzBuzz(num){
+    if(num % 3 === 0) return Fizz;
     return num;
 }
 
--- spec/fizz-buzz.spec.js
+++ spec/fizz-buzz.spec.js
@@ -2,6 +2,10 @@ const fizzBuzz = require('./fizz-buzz');
 
 describe('Curso Scrum - Fizz Buzz', () => {
     it('Retorna 1', () => {
-        expect(fizzBuzz(1)).toEqual(1)
+        expect(fizzBuzz(1)).toEqual(1);
+    })
+
+    it('Retorna Fizz se o numero passado for multiplo de 3', () =>{
+        expect(fizzBuzz(3)).toEqual('Fizz');
     })
 })
